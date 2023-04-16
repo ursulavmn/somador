68b3c3b9363c4233dbd494ba775d7af8641cdc4d
ursula <ursulavmn@gmail.com>
2023-04-16 12:37:35 -0300

    meu somador.js

---
+++ somador.js
@@ -0,0 +1,57 @@
+function soma(){
+    var parcela01 = parseInt (document.form_soma.parcela01.value);
+    var parcela02 = parseInt (document.form_soma.parcela02.value);
+    var resultado = parcela01 + parcela02;
+    return document.querySelector ("[name='resultado']").value = resultado
+}
+
+
+
+//todas as vezes que voce pega o valor do input ele vem em forma de string
+
+function subtrai(){
+    var subtraindo = parseInt (document.form_subtrair.subtraindo.value);
+    var subtraendo = parseInt (document.form_subtrair.subtraendo.value);
+    var diferenca = subtraindo - subtraendo;
+    return document.querySelector ("[name='diferenca']").value = diferenca
+}
+
+function multiplicaca(){
+    var parcela001 = parseInt (document.form_multiplicacao.parcela001.value);
+    var parcela002 = parseInt (document.form_multiplicacao.parcela002.value);
+    var multiplicacao_re = parcela001 * parcela002;
+    return document.querySelector ("[name='multiplicacao_re']").value = multiplicacao_re
+}
+
+
+
+function dividi(){
+    var parcela0001 = parseInt (document.form_dividir.parcela0001.value);
+    var parcela0002 = parseInt (document.form_dividir.parcela0002.value);
+    var dividir = parcela0001 / parcela0002;
+    return document.querySelector ("[name='dividir']").value = dividir
+}
+
+var nome = úrusla; 
+var idade = 21; 
+
+
+var comidafavorita; // var sem atribuir valor
+
+comidafavorita = 'pizza' // atribuir valor a comida favorita
+
+console.log(nome, idade, comidafavorita);
+
+
+// declarar 5 variaveis diferentes sem valor 
+
+var time = 'vasco',
+    professor, 
+    pais,
+    endereço,
+    rua;
+
+
+// tipos de dados 
+
+var nome = 'André' //string
